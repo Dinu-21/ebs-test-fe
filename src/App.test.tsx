d7f56eea9abcce126dbe616b836f25f0c0fb2457
Dinu <dinu@mediapark.com>
2023-05-19 16:27:18 +0300

    first commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,58 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App, { Fetcher, ICategory } from './App';
+import { act } from 'react-test-renderer';
+const categories: ICategory[] = [
+    { id: 'aksd', label: 'Some', productsCount: 0, parentId: 'parentId', children: [] },
+    {
+        id: 'ajasd',
+        label: 'Some2',
+        productsCount: 1,
+        parentId: 'parentId1',
+        children: [
+            {
+                id: 'nested',
+                label: 'Some3',
+                productsCount: 1,
+                parentId: 'parentId1',
+                children: [],
+            },
+        ],
+    },
+];
+test('renders categories', async () => {
+    const mock = jest.spyOn(Fetcher, 'get').mockImplementation(async () => categories);
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+    await act(async () => render(<App />) as any);
+
+    const firstCategory = screen.getByTestId(categories[0].id);
+    const secondCategory = screen.getByTestId(categories[1].id);
+    const nestedCategory = screen.getByTestId(categories[1].children[0].id);
+
+    expect(firstCategory).toBeInTheDocument();
+    expect(secondCategory).toBeInTheDocument();
+    expect(nestedCategory).toBeInTheDocument();
+
+    mock.mockRestore();
+});
+
+test('toggle nested categories', async () => {
+    const mock = jest.spyOn(Fetcher, 'get').mockImplementation(async () => categories);
+
+    await act(async () => render(<App />) as any);
+
+    const secondCategory = screen.getByTestId(categories[1].id);
+    const nestedCategory = screen.getByTestId(categories[1].children[0].id);
+
+    await act(async () => secondCategory.click());
+
+    expect(nestedCategory).not.toBeInTheDocument();
+
+    await act(async () => secondCategory.click());
+
+    const nestedCategory2 = screen.getByTestId(categories[1].children[0].id);
+
+    expect(nestedCategory2).toBeInTheDocument();
+
+    mock.mockRestore();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,105 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useEffect, useState } from 'react';
+import { Box, Grid, Typography } from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export interface ICategory {
+    id: string;
+    label: string;
+    productsCount: number;
+    parentId: null | string;
+    children: ICategory[];
 }
 
+interface ICategoryListProps {
+    subItems: ICategory[];
+    toggleExpand: (id: string) => void;
+    expand: (id: string) => boolean;
+}
+const CategoryList: React.FC<ICategoryListProps> = ({ subItems, toggleExpand, expand }) => {
+    return (
+        <>
+            {subItems.map(category => (
+                <Box key={category.id} style={{ margin: '5px 0 20px 20px' }} onClick={(e) => (e.stopPropagation(),toggleExpand(category.id))}>
+                    <Box data-testid={category.id} style={{ display: 'flex', gap: 20 }}>
+                        <Typography>
+                            <strong>Id</strong>: {category.id}
+                        </Typography>
+                        <Typography>
+                            <strong>Label</strong>: {category.label}
+                        </Typography>
+                        <Typography>
+                            <strong>Products</strong>: {category.productsCount}
+                        </Typography>
+                        <Typography>
+                            <strong>ParentId</strong>: {category.parentId}
+                        </Typography>
+                        {expand(category.id) ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+                    </Box>
+                    {expand(category.id) ? (
+                        <CategoryList toggleExpand={toggleExpand} expand={expand} subItems={category.children} />
+                    ) : (
+                        ''
+                    )}
+                </Box>
+            ))}
+        </>
+    );
+};
+export const Fetcher = {
+    async get(url: string): Promise<ICategory[]> {
+        let result: ICategory[] = [];
+        try {
+            result = await fetch(url).then(r => r.json());
+        } catch (e) {}
+        return result;
+    },
+};
+const Categories = () => {
+    const [register, setRegister] = useState<Record<string, boolean>>({});
+    const [data, setData] = useState<ICategory[]>([]);
+
+    useEffect(() => {
+        async function fetch() {
+            const items = await Fetcher.get('http://localhost:3001/api/v1/categories');
+            setData(items);
+        }
+        fetch();
+    }, []);
+
+    const toggleExpand = useCallback(
+        (id: string) => {
+            if (!(id in register)) {
+                register[id] = false;
+            } else register[id] = !register[id];
+            setRegister({ ...register });
+        },
+        [register]
+    );
+
+    const expand = useCallback(
+        (id: string) => {
+            const value = register[id];
+            if (value === undefined) return true;
+            return !!value;
+        },
+        [register]
+    );
+
+    return <CategoryList subItems={data} expand={expand} toggleExpand={toggleExpand} />;
+};
+
+function App() {
+    return (
+        <Box style={{ paddingTop: 50 }}>
+            <Grid container spacing={3}>
+                <Grid item lg={2}></Grid>
+                <Grid item lg={8} style={{ boxShadow: '0px 0px 38px -25px rgba(0,0,0,0.36)', height: '100vh' }}>
+                    <Categories />
+                </Grid>
+                <Grid item lg={2}></Grid>
+            </Grid>
+        </Box>
+    );
+}
 export default App;
